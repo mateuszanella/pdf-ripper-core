91353f1c9b79b3d95ca24d60247c299fcad039ef
mateuszanella <mateusblk1@gmail.com>
2026-05-14 14:51:22 -0300

    commit test pdf

--- examples/read_document.cpp
+++ examples/read_document.cpp
@@ -120,7 +120,8 @@ int main(int argc, char **argv)
 {
     const std::filesystem::path path = (argc > 1)
                                            ? std::filesystem::current_path() / std::filesystem::path{argv[1]}
-                                           : std::filesystem::current_path() / "../example/test.pdf";
+                                           // This assumes you are running the executable from the `build` directory.
+                                           : std::filesystem::current_path() / "../examples/pdfs/test.pdf";
 
     try
     {
