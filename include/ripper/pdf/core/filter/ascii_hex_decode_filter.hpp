273d19bcb2e6f001355122bfc36db73cfdb788df
mateuszanella <mateusblk1@gmail.com>
2026-08-07 09:19:18 -0300

    add ascii codecs

---
+++ include/ripper/pdf/core/filter/ascii_hex_decode_filter.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ripper/pdf/core/filter/stream_filter.hpp"
+
+namespace ripper::pdf::core
+{
+
+class ascii_hex_decode_filter final : public stream_filter
+{
+public:
+    [[nodiscard]] std::vector<std::byte>
+    decode(std::span<const std::byte> input,
+           const dictionary_object* params = nullptr) const override;
+
+    [[nodiscard]] std::vector<std::byte>
+    encode(std::span<const std::byte> input,
+           const dictionary_object* params = nullptr) const override;
+};
+
+} // namespace ripper::pdf::core
